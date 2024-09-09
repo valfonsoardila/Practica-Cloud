049cb6e4b63b814ebfb50e0db964dc864dbffb2e
Victor Alfonso Ardila Montalban <valfonsoardila@unicesar.edu.co>
2024-09-09 18:52:55 -0500

    Scripts y comandos

---
+++ QUERY TABLA USER.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Cloud;
+GO
+USE Cloud;
+GO
+
+CREATE TABLE Cloud (
+    Id INT,
+    Name NVARCHAR(100)
+);
+GO
+
+INSERT INTO Cloud (Id, Name) VALUES (1003377848, 'Victor Ardila');
